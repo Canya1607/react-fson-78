8399f39ed8457266b8675fc888fcb3f7886db3b7
Canya1607 <hnenniy1610@gmail.com>
2023-06-13 22:38:46 +0300

    End of lesson 1-1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import './App.css';
+import { Product } from ".";
+import "./App.css";
+
+const products = [
+  {
+    id: "p-1",
+    showTitle: false,
+    imageUrl: "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640",
+    price: 10.99,
+  },
+  {
+    id: "p-2",
+    showTitle: true,
+    imageUrl: "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640",
+    price: 10.99,
+  },
+  {
+    id: "p-3",
+    showTitle: true,
+    imageUrl: "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640",
+    price: 100.11,
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      Hello React
+      {products.map((product, index) => (
+        <Product
+          key={product.id}
+          showTitle={product.showTitle}
+          imageUrl={product.imageUrl}
+          productPrice={product.price && product.price} // undefined null '' 0
+        />
+      ))}
     </div>
   );
 }
